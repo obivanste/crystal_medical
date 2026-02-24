de170574a8bb35ae4b26e7c5ba26af376e062095
Stevan Markovic <stevanm.biz@gmail.com>
2026-02-24 23:16:09 +0100

    Changed contact to office@crystalmedical.rs

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -40,7 +40,7 @@ $comments = stripslashes($comments);
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "mihaila.jaksic@gmail.com";
+$address = "office@crystalmedical.rs";
 
 
 // Configuration option.
